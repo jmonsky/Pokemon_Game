8e15390bc272a6cb4bdec487573eb72c8f4bfd6d
jmonsky <jake@monsky.info>
2018-10-21 20:30:23 -0500

    Deleted a potentially dangerous file, was using it to generate all the files, but dont want anyone to accidentally overwrite all the stats

--- createBlankPokedataFiles.py
+++
@@ -1,9 +0,0 @@
-from pokeName import *
-import json
-for ID in range(1,803):
-	with open(".\\Data\\Pokemon\\"+getPokeFile(ID)+".json", "w+") as FILE:
-		with open("base.json", "r") as file:
-			baseInfo = json.loads(file.read())
-		baseInfo["ID"] = ID
-		baseInfo["Name"] = getPokeByID(ID)
-		json.dump(baseInfo, FILE, indent = 4)
